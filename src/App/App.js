ca4aaa3b1614e85c8096318048d999989096ec46
Tanjie McMeans <tanjmcmeans01@gmail.com>
2019-06-14 16:40:28 -0400

    Add controlled form for saving pokemon types

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,25 @@
 import React, { useState } from 'react';
 
 /* Search Form */
-const SearchForm = () => {
-  const [type, getPokemonType] = useState({
-    type: ''
+const SearchForm = props => {
+  const [type, setPokemonType] = useState({
+    type: 'all'
   });
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(`filtering by pokemon type: ${type}`);
+    props.filterPokemonByType(type);
+  };
+
   return (
-    <form action="">
-      <input type="text" />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        onChange={e => setPokemonType(e.target.value)}
+        placeholder="filter by Pokemon type"
+        value={type}
+      />
       <button>Search</button>
     </form>
   );
@@ -65,12 +77,18 @@ const App = () => {
     }
   };
 
+  const filterPokemonByType = type => {
+    console.log(type);
+    // const filteredPokemon = pokemon.filter(() => )
+    // savePokemon(filteredPokemon);
+  };
+
   if (pokemon.length) {
     return (
       <div className="app">
         <h1>Pokedex</h1>
         <p>Search by type</p>
-        <SearchForm />
+        <SearchForm filterPokemonByType={filterPokemonByType} />
         <PokeList pokemon={pokemon} />
         <button
           onClick={e => {
@@ -97,15 +115,13 @@ export default App;
 // Publically accessible API- https://pokeapi.co/api/v2/type/{type}
 //Describes type of data user will receive along with a button
 //When button is clicked it calls to the api, and displays the data on screen replacing the description text with a table or list type view
-//
+//Allow a user to request more items somehow. This should add to the current list, rather than replacing it
 
 //WIP
-// 2. Allow a user to request more items somehow. This should add to the current list, rather than replacing it
 // 3. Add an input somewhere that, as a user types into it, filters the currently showing results (no need to fetch things based on this).
 // 4. When clicking on an item in this list or table view, navigate to an item specific view that shows a bit more detail.
-
 // Please DO use a routing library (react-router, router-5, whatever-other-router).
 
-// ## How to submit
+//HOW TO SUBMIT
 // Send us an archive (zip, or the like) of your code sample, including the git history.
 // As a heads up, the next phase of the interview will contain a discussion of your work and process, as well as working with an engineer here to expand on this application.
