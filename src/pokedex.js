a0e17a365adaed1e8dc62d714411883ab87c9e8c
Tanjie McMeans <tanjmcmeans01@gmail.com>
2019-06-28 19:27:54 -0400

    Comment out pokedex.js file

--- src/pokedex.js
+++ src/pokedex.js
@@ -1,33 +1,33 @@
-class Pokedex {
-  constructor(newPokeData) {
-    this.pokeData = newPokeData;
-    this.nextDataSet = this.newPokeData.next;
-    this.pokemonList = [];
-  }
+// class Pokedex {
+//   constructor(newPokeData) {
+//     this.pokeData = newPokeData;
+//     this.nextDataSet = this.newPokeData.next;
+//     this.pokemonList = [];
+//   }
 
-  cleanPokeData = async uncleanPokeData => {
-    let responses = await Promise.all(
-      uncleanPokeData.results.map(currentPoke => fetch(currentPoke.url))
-    );
-    let cleanPokeData = await Promise.all(
-      responses.map(result => result.json())
-    );
+//   cleanPokeData = async uncleanPokeData => {
+//     let responses = await Promise.all(
+//       uncleanPokeData.results.map(currentPoke => fetch(currentPoke.url))
+//     );
+//     let cleanPokeData = await Promise.all(
+//       responses.map(result => result.json())
+//     );
 
-    addPokemonToList(cleanPokeData);
-  };
+//     addPokemonToList(cleanPokeData);
+//   };
 
-  addPokemonToList = cleanPokeData => {
-    cleanPokeData.forEach(pokemon => this.pokemonList.push(pokemon));
-  };
+//   addPokemonToList = cleanPokeData => {
+//     cleanPokeData.forEach(pokemon => this.pokemonList.push(pokemon));
+//   };
 
-  findByType = type => {
-    const filteredPokemon = pokemon.filter(poke => {
-      for (let i = 0; i < poke.types.length; i++) {
-        // const { name } = poke.type[i].type;
-        console.log(poke.types[i].type);
-      }
-    });
-  };
-}
+//   findByType = type => {
+//     const filteredPokemon = pokemon.filter(poke => {
+//       for (let i = 0; i < poke.types.length; i++) {
+//         // const { name } = poke.type[i].type;
+//         console.log(poke.types[i].type);
+//       }
+//     });
+//   };
+// }
 
-export default Pokedex;
+// export default Pokedex;
